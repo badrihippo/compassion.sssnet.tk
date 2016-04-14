667d1e35d76b22aff57886bf7d272b3812c50d0e
Hippo <badrihippo@gmail.com>
2016-04-14 13:33:27 +0530

    Updated app to work on OpenShift

--- compassion/app.py
+++ compassion/app.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
-app.config.from_object('config.DevelopmentConfig')
+try:
+    app.config.from_pyfile('../instance/config.py')
+except:
+    print 'Using development config...'
+    app.config.from_object('config.DevelopmentConfig')
 
 db = MongoEngine(app)
--- config.py
+++ config.py
@@ -1,13 +1,25 @@
+import os
 class Config(object):
     DEBUG = False
 
 class DevelopmentConfig(Config):
     SECRET_KEY = 'This is supposedly secret'
-    DEBUG = True
-    MONGODB_SETTINGS = {'db': 'compassion'}
 
     # The following keys are from a test account I created. Please
     # replace them with your personal site keys before deploying on the
     # actual website, or else they won't work.
     RECAPTCHA_PUBLIC_KEY = '6LfqIxMTAAAAAFBX7k0jfutFr1gRFkD8lF7JYbuC'
     RECAPTCHA_PRIVATE_KEY = '6LfqIxMTAAAAAG6kDJu0xiy4uTgLjggbnFd3fFRG'
+
+    if os.environ.get('OPENSHIFT_APP_NAME') is not None: # OpenShift setup
+        print 'Detected OpenShift app. Loading parameters...'
+        MONGODB_DB = os.environ.get('OPENSHIFT_APP_NAME')
+        MONGODB_HOST = os.environ.get('OPENSHIFT_MONGODB_DB_HOST')
+        MONGODB_PORT = os.environ.get('OPENSHIFT_MONGODB_DB_PORT', 0)
+        MONGODB_USERNAME = os.environ.get('OPENSHIFT_MONGODB_DB_USERNAME')
+        MONGODB_PASSWORD = os.environ.get('OPENSHIFT_MONGODB_DB_PASSWORD')
+        
+        DEBUG = False
+    else:
+        DEBUG = True
+        MONGODB_SETTINGS = {'db': 'compassion'}
